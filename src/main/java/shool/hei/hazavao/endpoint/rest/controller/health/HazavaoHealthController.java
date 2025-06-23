d5746dd591c84cd50c25bd35362f23009c580582
Harentsoa <hei.harentsoa@gmail.com>
2025-06-23 08:44:02 +0300

    fix: endpoint

--- src/main/java/shool/hei/hazavao/endpoint/rest/controller/health/HazavaoHealthController.java
+++ src/main/java/shool/hei/hazavao/endpoint/rest/controller/health/HazavaoHealthController.java
@@ -3,6 +3,7 @@ package shool.hei.hazavao.endpoint.rest.controller.health;
 import com.shahabkondri.chatgpt.api.model.ChatGptRequest;
 import com.shahabkondri.chatgpt.api.model.MessageRole;
 import com.shahabkondri.chatgpt.api.model.TextCompletionModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,13 +13,13 @@ import java.util.List;
 
 @RestController
 public class HazavaoHealthController {
-
+    @Autowired
     private ChatService chatService;
 
     @GetMapping("/hazavao")
-    public String hazavao(@RequestParam String prompt) {
+    public String hazavao(@RequestParam String teny) {
         TextCompletionModel model = TextCompletionModel.GPT_3_5_TURBO;
-        List<ChatGptRequest.Message> messages = List.of(new ChatGptRequest.Message(MessageRole.USER, "donner la definition en Malagasy de"+ prompt));
+        List<ChatGptRequest.Message> messages = List.of(new ChatGptRequest.Message(MessageRole.USER, "donner la definition en Malagasy de" + teny));
         ChatGptRequest request = new ChatGptRequest(model, messages);
         return chatService.chat(request);
     }
